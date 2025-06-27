b61662cb96e71f5434b63129846d1f135ed2d4a3
B14ck <52348159+blackangiliq@users.noreply.github.com>
2025-06-27 23:31:28 +0300

    Update linux_installer.sh

--- linux_installer.sh
+++ linux_installer.sh
@@ -9,19 +9,7 @@ pip3 install requests aiohttp
 # Download the script
 echo "📥 Downloading script..."
 curl -s -L "https://raw.githubusercontent.com/blackangiliq/server_distroyer/main/linux_load_test.py" -o stress_test.py
-
-# Check if download was successful
-if [ ! -s stress_test.py ]; then
-    echo "❌ Download failed. Checking for local file..."
-    if [ -f "brutal_server_stress_test.py" ]; then
-        echo "✅ Using local brutal_server_stress_test.py file..."
-        cp brutal_server_stress_test.py stress_test.py
-    else
-        echo "❌ Could not download the script and no local file found."
-        exit 1
-    fi
-fi
-
+sudo apt install libgcc1
 # Run the script
 echo "🚀 Starting stress test..."
 python3 stress_test.py 
